fad96e49512b91bd41982e8710e8bd1eb2c55fed
shehan9400 <149374187+shehan9400@users.noreply.github.com>
2024-12-25 21:01:18 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "DrJ1kLwI#jMY59MUX4VcJMDKVRwgpDoZwicz_Tl1GUtMV7NQEzA0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
